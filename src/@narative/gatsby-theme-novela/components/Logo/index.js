e84650bdf9e373b2d0aaab6db49d826dc5cb383f
sunny <sunnychoudhury007@gmail.com>
2019-10-17 11:48:02 +0530

    update logo

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Link} from 'gatsby';
+export default function Logo(){
+    return(
+        <Link to ='/'><h1 style={{fontSize:'2.5rem'}}>itzsunny</h1></Link>
+    )
+}
